e3f5b7af5dabaf8f7b994eb9fb572b73446d1ddc
prajwal <prajwalthete98@gmail.com>
2023-05-07 22:25:01 +0530

    Portf

---
+++ pages/resume.jsx
@@ -0,0 +1,122 @@
+import React from 'react';
+import Head from 'next/head';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+
+const resume = () => {
+  return (
+    <>
+      <Head>
+        <title>Clint | Resume</title>
+        <meta
+          name='description'
+          content='I’m a full-stack developer specializing in building (and occasionally designing) exceptional digital experiences.'
+        />
+        <link rel='icon' href='/fav.png' />
+      </Head>
+
+      <div className='max-w-[940px] mx-auto p-2 pt-[120px]'>
+        <h2 className='text-center'>Resume</h2>
+        <div className='bg-[#d0d4d6] my-4 p-4 w-full flex justify-between items-center'>
+          <h2 className='text-center'>Prajwal Thete</h2>
+          <div className='flex'>
+            <a
+              href='https://www.linkedin.com/in/prajwal-thete-7389601a0/'
+             
+              target='_blank'
+              rel='noreferrer'
+            >
+              <FaLinkedinIn size={20} style={{ marginRight: '1rem' }} />
+            </a>
+            <a
+              href='https://github.com/prajwalthete'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <FaGithub size={20} style={{ marginRight: '1rem' }} />
+            </a>
+          </div>
+        </div>
+        <div className='text-center py-4 text-xl font-bold uppercase tracking-wider'>
+          <div className='hidden sm:block'>
+            <p>
+              Proven Leadership <span className='px-1'>|</span> Web Development{' '}
+              <span className='px-1'>|</span> Complex Problem Solving
+            </p>
+          </div>
+          <div className='block sm:hidden'>
+            <p>Proven Leadership</p>
+            <p className='py-2'>Web Development</p>
+            <p>Complex Problem Solving</p>
+          </div>
+        </div>
+        <p>
+          Analytical, innovative, and motivated web development professional
+          with experience in customer service, team leadership, and
+          organizational effectiveness in fast-paced and challenging
+          environments. Adept at developing strategies and driving streamlined
+          operations. Diverse analytical skills, team collaboration, and
+          relationship building. Consummate professional, and motivated leader,
+          with solid interpersonal abilities and complex problem-solving skills.
+          Effective and proven track record of critical thinking, idea
+          generation, and optimizing efficiencies.
+        </p>
+
+        {/* Skills */}
+        <div className='text-center py-4'>
+          <h5 className='text-center underline text-[18px] py-2'>Skills</h5>
+          <p className='py-2'>
+            <span className='font-bold'>Technical Skills</span>
+            <span className='px-2'>|</span>Full-Stack Developer
+            <span className='px-2'>|</span> HTML
+            <span className='px-2'>|</span>CSS
+            <span className='px-2'>|</span>Javascript
+            <span className='px-2'>|</span>React
+            <span className='px-2'>|</span>Next JS
+            <span className='px-2'>|</span>SQL
+            <span className='px-2'>|</span>NoSQL
+            <span className='px-2'>|</span>bootstrap
+            <span className='px-2'>|</span>Tailwind
+            <span className='px-2'>|</span> java-spring-boot
+            <span className='px-2'>|</span> RESTAPI
+          </p>
+          <p className='py-2'>
+            <span className='font-bold'>Amazon Web Services</span>
+            <span className='px-2'>|</span>Amazon Web Services Cloud
+            Practitioner
+          </p>
+        </div>
+
+        <h5 className='text-center underline text-[18px] py-4'>
+          Professional Experience
+        </h5>
+       
+
+
+        {/* Personal Experience */}
+        <div className='py-6'>
+          <p className='italic'>
+            <span className='font-bold italic'>
+              Teaching & Mentoring Web Development
+            </span>
+            <span className='px-2'>|</span>Mumbai India 
+          </p>
+          <p className='py-1 italic'>Prajwal- Youtube Channel (2021 - Current)</p>
+          <ul className='list-disc list-outside px-7 py-1 leading-relaxed'>
+            <li>
+              In 2021 I started “Prajwal Thete”, a YouTube channel focused on
+              sharing what I’m learning in web development, specifically
+              front-end development.
+            </li>
+            <li>
+              Private 1 on 1 Zoom coaching calls related to web development.
+              More specifically debugging, teaching basic web development
+              concepts and code reviews.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default resume;
